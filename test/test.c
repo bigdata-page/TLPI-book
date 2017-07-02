0d78fcdf1e0b2e42485c1e932cebd3b0289daa7f
jun <879933274@qq.com>
2017-07-02 12:19:48 +0800

    the source of the linux programming interface

---
+++ test/test.c
@@ -0,0 +1,29 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "tlpi_hdr.h"
+
+int main(int argc, char *argv[])
+{
+    int i = 10 ;
+    printf("This is the test\n");
+   
+    errMsg("Test the ErrMsg %d\n", errno);
+    
+    return 0;
+}
+sockaddr
+sockaddr_in
+socket
+in_addr
+fopen
+fwrite
+fread(p,m,n,s)
+lseek
+getservbyname
+getaddrinfo
+
+
+syslog
+
+
